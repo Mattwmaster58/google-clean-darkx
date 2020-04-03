f6fec32a82e04f4d62973d8136a7418b09260169
Matt Marcus <mattmarcus58@gmail.com>
2020-04-03 15:37:39 -0600

    update build.py, reformat every file

--- build.py
+++ build.py
@@ -1,10 +1,24 @@
+from datetime import datetime
+from pathlib import Path
+from textwrap import dedent
+
 """
 For building main theme
 """
 
+OUTPUT_FILE_NAME = 'google-clean-darkx.user.css'
+
 
 def main():
-    for file in Path('./css'):
+    with open(OUTPUT_FILE_NAME, 'w') as out:
+        out.write(dedent(f'''\
+        /*
+        Built on {datetime.utcnow()}
+        DO NOT MODIFY THIS FILE DIRECTLY! Instead, modify the appropriate CSS file in the ./css directory
+        */\n\n'''))
+        for file in Path('./css').iterdir():
+            with open(file) as inf:
+                out.write(inf.read())
 
 
 if __name__ == '__main__':
