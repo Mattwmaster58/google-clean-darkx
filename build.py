d4ec35c9dd587df96d427d05f7144b3e7218b873
Matt Marcus <mattmarcus58@gmail.com>
2020-04-11 22:20:08 -0600

    update accounts sign in stuff

--- build.py
+++ build.py
@@ -32,39 +32,38 @@ def main(version_bump):
     version = dict(
         zip(['major', 'minor', 'patch'], re.search(r'@version\s*(\d+)\.(\d+)\.(\d+)', metadata_content).groups(),)
     )
-    print(f'bumping {version_bump} component of version string ({version[version_bump]}+1)')
-    version[version_bump] = str(int(version[version_bump]) + 1)
-    metadata_content = re.sub(
-        r'@version(?P<whitespace>\s*)(\d+)\.(\d+)\.(\d+)',
-        fr'@version\g<whitespace>{".".join(version.values())}',
-        metadata_content,
-        re.MULTILINE,
-    )
-    with open(METADATA_FILE, 'w') as new_meta:
-        new_meta.write(metadata_content)
-    to_write = ''
-    # we want main and global styles to be overridden not the other way around
-    for file in sorted(Path('./css').iterdir(), key=lambda f: 'main' in f.name or 'global' in f.name):
-        with open(file) as inf:
-            to_write += inf.read()
-    to_write = compress(to_write, preserve_exclamation_comments=False)
-    copy(to_write)
-    with open(OUTPUT_FILE_NAME, 'w') as out:
-        out.write(metadata_content)
-        out.write(
-            dedent(
-                f'''\
+    if version_bump:
+        print(f'bumping {version_bump} component of version string ({version[version_bump]}+1)')
+        version[version_bump] = str(int(version[version_bump]) + 1)
+        metadata_content = re.sub(
+            r'@version(?P<whitespace>\s*)(\d+)\.(\d+)\.(\d+)',
+            fr'@version\g<whitespace>{".".join(version.values())}',
+            metadata_content,
+            re.MULTILINE,
+        )
+        with open(METADATA_FILE, 'w') as new_meta:
+            new_meta.write(metadata_content)
+    else:
+        print(f'not bumping version, maintaining v{".".join(version.values())}')
+    to_write = metadata_content
+    to_write += dedent(f'''\
             /*
             Built on {datetime.utcnow()}
             DO NOT MODIFY THIS FILE DIRECTLY! Instead, modify the appropriate CSS file in the ./css directory
-            */\n\n'''
-            )
-        )
+            */\n\n''')
+    css_main = ''
+    # we want main and global styles to be overridden not the other way around
+    for file in sorted(Path('./css').iterdir(), key=lambda f: 'main' in f.name or 'global' in f.name):
+        with open(file) as inf:
+            css_main += inf.read()
+    to_write += compress(css_main, preserve_exclamation_comments=False)
+    copy(to_write)
+    with open(OUTPUT_FILE_NAME, 'w') as out:
         out.write(to_write)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser('build')
-    parser.add_argument('--bump', '-b', type=str, choices=['major', 'minor', 'patch'], default='patch')
+    parser.add_argument('--bump', '-b', type=str, choices=['major', 'minor', 'patch'], default=None)
     parsed = parser.parse_args()
     main(version_bump=parsed.bump)
